0c7ad028f884be532b81a3f646cbdeff7d5d313c
Harsh Desai <desaiharsh512@gmail.com>
2026-01-31 20:03:24 +0530

    BE fix: reorder classroom routes to fix route matching issue in classroom.routes

--- backend/routes/classroom.routes.js
+++ backend/routes/classroom.routes.js
@@ -5,7 +5,7 @@ const classroomController = require("../controllers/classroom.controller.js")
 
 router.post("/", classroomController.createClassroom)
 router.post("/join", classroomController.joinClassroom)
-router.get("/:classroomId/:userId", classroomController.getClassroom)
 router.get("/:classroomId/participants", classroomController.getParticipants)
+router.get("/:classroomId/:userId", classroomController.getClassroom)
 
 module.exports = router
