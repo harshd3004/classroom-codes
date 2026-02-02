918c25d18fe3355abfa3f69bad520a89ec246211
Harsh Desai <desaiharsh512@gmail.com>
2026-02-02 11:29:09 +0530

    feat : Implement Monaco Editor

--- frontend/src/components/EditorPanel.jsx
+++ frontend/src/components/EditorPanel.jsx
@@ -1,6 +1,22 @@
+import Editor from '@monaco-editor/react';
+
 function EditorPanel() {
   return (
-    <div>EditorPanel</div>
+    <div className='h-full flex flex-col mx-1'>
+      {/* Topbar */}
+      <div className="h-12 border-b flex items-center px-4 bg-slate-50 ">
+        <span>Editor</span>
+      </div>
+
+      {/* Editor */}
+      <div className='flex-1 border'>
+        <Editor
+          height="90vh"
+          defaultLanguage="javascript"
+          defaultValue="console.log('Hello world!')"
+        />
+      </div>
+    </div>
   )
 }
 
