fa22ea62d5e31530c9515ae21b49d792b5071c0b
Harsh Desai <desaiharsh512@gmail.com>
2026-02-02 13:00:25 +0530

    FE: Add .env file

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 const api = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: API_BASE_URL,
     headers: {
         "Content-Type": "application/json"
     }
